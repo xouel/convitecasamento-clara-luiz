adcce607e3d0bd89068d8b361078d8e135627d57
Joel Junior <joeljunior0912@gmail.com>
2026-08-29 02:40:52 -0300

    Add files via upload

--- src/config/wedding.ts
+++ src/config/wedding.ts
@@ -56,7 +56,7 @@ export const weddingConfig: WeddingConfig = {
   },
   rsvp: {
     deadlineDisplay: "25 de setembro",
-    url: "https://wa.me/5532988759890",
+    url: "https://api.whatsapp.com/send?phone=5532988759890&text=%F0%9F%92%8D%20CONFIRMA%C3%87%C3%83O%20DE%20PRESEN%C3%87A%20%E2%80%94%20CLARA%20%26%20LUIZ%20CLAUDIO%20%F0%9F%A4%8D%0A%0AOl%C3%A1!%20Ficamos%20muito%20felizes%20em%20ter%20voc%C3%AA%20fazendo%20parte%20desse%20momento%20t%C3%A3o%20especial%20da%20nossa%20hist%C3%B3ria.%20%F0%9F%A5%B0%0A%0ANosso%20casamento%20est%C3%A1%20se%20aproximando%20e%2C%20para%20organizarmos%20tudo%20com%20muito%20carinho%2C%20pedimos%20que%20confirme%20sua%20presen%C3%A7a%20preenchendo%20as%20informa%C3%A7%C3%B5es%20abaixo%3A%0A%0A%E2%9C%A8%20Nomes%20das%20pessoas%20que%20ir%C3%A3o%20ao%20casamento%3A%0A%0A%E2%B8%BB%0A%0A%E2%B8%BB%0A%0A%E2%B8%BB%0A%0A%F0%9F%93%85%20Data%3A%2017%20de%20outubro%20de%202026%0A%F0%9F%95%9A%20Hor%C3%A1rio%3A%2011h%0A%F0%9F%93%8D%20Local%3A%20Espa%C3%A7o%20Vilela%0A%0AAgradecemos%20desde%20j%C3%A1%20pelo%20carinho%20e%20por%20celebrar%20esse%20dia%20t%C3%A3o%20importante%20conosco!%20%F0%9F%A4%8D%0A%0ACom%20carinho%2C%0AClara%20%26%20Luiz%20Claudio%20%F0%9F%92%8D",
   },
   gifts: {
     url: "https://listapresente-clara-luiz.vercel.app/",
